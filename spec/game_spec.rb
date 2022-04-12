9788933e7ed891b01cf96bd251aae31408460d3f
Stirling Hostetter <stirhoss@icloud.com>
2022-04-12 17:11:34 -0500

    Add test for lowest total score

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -19,3 +19,20 @@ describe 'highest total score' do
     expect(@stat_tracker.highest_total_score).to eq 6
   end
 end
+
+describe 'lowest total score' do
+  before:each do
+    game_path = './test_data/games.csv'
+    team_path = './test_data/teams.csv'
+    game_teams_path = './test_data/game_teams.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+  it 'returns the lowest sum of the teams goals' do
+    expect(@stat_tracker.lowest_total_score).to eq 1
+  end
+end
