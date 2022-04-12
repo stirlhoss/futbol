5d684997fce1fe215d655da3f4103f9a7a0a970f
Stirling Hostetter <stirhoss@icloud.com>
2022-04-12 17:13:24 -0500

    Add tests for percentage home and visitor wins

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -36,3 +36,37 @@ describe 'lowest total score' do
     expect(@stat_tracker.lowest_total_score).to eq 1
   end
 end
+
+describe 'percentage_home_wins' do
+  before:each do
+    game_path = './test_data/games.csv'
+    team_path = './test_data/teams.csv'
+    game_teams_path = './test_data/game_teams.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+  it 'returns the percentage of home wins' do
+    expect(@stat_tracker.percentage_home_wins).to eq 65.00
+  end
+end
+
+describe 'percentage_visitor_wins' do
+  before:each do
+    game_path = './test_data/games.csv'
+    team_path = './test_data/teams.csv'
+    game_teams_path = './test_data/game_teams.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+  it 'returns the percentage of visitor wins' do
+    expect(@stat_tracker.percentage_visitor_wins).to eq 35.00
+  end
+end
