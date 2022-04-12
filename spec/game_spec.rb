146ed5d71d1af9386e1828c576decbf5da7bfc83
Stirling Hostetter <stirhoss@icloud.com>
2022-04-12 16:19:11 -0500

    Add test for highest total score

---
+++ spec/game_spec.rb
@@ -0,0 +1,21 @@
+require 'pry'
+require 'rspec'
+require './lib/stat_tracker'
+# require './test_data/games.csv'
+
+describe 'highest total score' do
+  before:each do
+    game_path = './test_data/games.csv'
+    team_path = './test_data/teams.csv'
+    game_teams_path = './test_data/game_teams.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+  it 'returns the highest sum of the teams goals' do
+    expect(@stat_tracker.highest_total_score).to eq 6
+  end
+end
