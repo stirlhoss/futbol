180933fd4de8d856801d22ea3299dc40f2c09460
Caden Jarrett <caden.jarrett99@gmail.com>
2022-04-18 11:00:05 -0600

    Fix: modified test results to match what format is expected

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -96,21 +96,21 @@ describe "StatTracker functionality" do
   describe 'percentage_home_wins' do
     it 'returns the percentage of home wins' do
 
-      expect(@stat_tracker.percentage_home_wins).to eq 43.50
+      expect(@stat_tracker.percentage_home_wins).to eq 0.44
     end
   end
 
   describe 'percentage_visitor_wins' do
     it 'returns the percentage of visitor wins' do
 
-      expect(@stat_tracker.percentage_visitor_wins).to eq 36.11
+      expect(@stat_tracker.percentage_visitor_wins).to eq 0.36
     end
   end
 
   describe 'percentage_draws' do
     it 'returns the percentage of games that end in draws' do
 
-      expect(@stat_tracker.percentage_ties).to eq 20.39
+      expect(@stat_tracker.percentage_ties).to eq 0.20
     end
   end
 
@@ -118,12 +118,12 @@ describe "StatTracker functionality" do
     it 'returns a hash of season_id keys and all games associated with them' do
 
     expect(@stat_tracker.count_of_games_by_season).to eq({
-      20122013=>806,
-      20162017=>1317,
-      20142015=>1319,
-      20152016=>1321,
-      20132014=>1323,
-      20172018=>1355
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
       })
     end
   end
@@ -138,12 +138,12 @@ describe "StatTracker functionality" do
   describe 'average_goals_by_season' do
     it 'averages the total number of goals and stores them in a hash by season' do
       expect(@stat_tracker.average_goals_by_season).to eq({
-        20122013=>4.12,
-        20162017=>4.23,
-        20142015=>4.14,
-        20152016=>4.16,
-        20132014=>4.19,
-        20172018=>4.44
+        "20122013"=>4.12,
+        "20162017"=>4.23,
+        "20142015"=>4.14,
+        "20152016"=>4.16,
+        "20132014"=>4.19,
+        "20172018"=>4.44
         })
     end
   end
@@ -177,13 +177,12 @@ describe "StatTracker functionality" do
   it "can name team with fewest tackles" do
     expect(@stat_tracker.fewest_tackles("20132014")).to eq("Atlanta United")
   end
-end
 
 #====================
 #Team spec tests
 #====================
 
- describe 'team tests' do
+  describe 'team tests' do
   it "returns the info of a team" do
     expect(@stat_tracker.team_info("1")).to eq({
       abbreviation: "ATL",
@@ -193,6 +192,7 @@ end
       team_id: "1",
       teamname: "Atlanta United"
       })
+    end
   end
 
   it "returns the best season for a given team" do
@@ -204,7 +204,7 @@ end
   end
 
   it "can return a teams average win percentage of all games for a team" do
-    expect(@stat_tracker.average_win_percentage("6")).to eq(49.22)
+    expect(@stat_tracker.average_win_percentage("6")).to eq(0.49)
   end
 
   it "can return the highest number of goals a particular team has scored in a single game" do
@@ -216,10 +216,10 @@ end
   end
 
   it "can return the name of the opponent that has the lowest win percentage against the given team" do
-    expect(@stat_tracker.favorite_opponent("5")).to eq("14")
+    expect(@stat_tracker.favorite_opponent("5")).to eq("Washington Spirit FC")
   end
 
   it "can return the Name of the opponent that has the highest win percentage against the given team." do
-    expect(@stat_tracker.rival("5")).to eq("6")
+    expect(@stat_tracker.rival("5")).to eq("San Jose Earthquakes")
   end
  end
